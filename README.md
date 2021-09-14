72f422c2b0786d27f25ba9c2ce03f8230fddf1b8
DavidFGitH <kamidur4@gmail.com>
2021-09-13 20:24:22 -0400

    Challenge

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # UFOs
 Finding UFOs with Javascript
+
+## Overview of Project
+
+To create a website that allows the user to analyze UFO sighting, with options to filter and isolate multiple criteria.
+
+## Results
+
+After opening the website, we are greeted with the following page:
+
+![UFOs1](githubcomDavidFGitH)
+
+To start filtering all the data available, we would scroll down to where we see all the options for filtering:
+
+![UFOs2](githubcomDavidFGitH)
+
+Currently, we can scroll down the page to see all the data currently available to us, but in order to filter the data we must enter values into the filter. In order to engage the filter, we will enter a value into one of the blank filters as shown below:
+
+![UFOs3](githubcomDavidFGitH)
+
+After hitting enter, we can see that the values in the table included have now changed to just include the values that we entered in the filter.
+
+![UFOs4](githubcomDavidFGitH)
+
+We can further refine this search by adding values into other empty filter fields:
+
+![UFOs5](githubcomDavidFGitH)
+
+If we ever need to clear the filters, we just delete what was entered into the filter and the table will reset itself to our needs.
+
+## Summary
+
+Overall, the website in it's current form provides a basic options to filter the data to isolate the data of UFO sightings using the preset filters and criteria provided by the website. However, just filtering the data doesn't offer a lot of value in terms of analysis, and it becomes difficult to find the scale of importance for things like specific cities which might experience more sightings than others, or whether there might be patterns that emerge, or what the pool of options and values are in the data collected. There are lots of options to add more functionality to the website, whether it is providing a dropdown in the filter instead of having to type exactly what to filter for providing more convenient ways for filtering as well as showing all the options that are available. Adding a count of results could also help a lot in terms of easily quantifying the results of specifying criteria.
--- static/js/app.js
+++ static/js/app.js
@@ -19,32 +19,64 @@ function buildTable(data) {
     Object.values(dataRow).forEach((val) => {
       let cell = row.append("td");
       cell.text(val);
-      }
-    );
+    });
   });
 }
 
-function handleClick() {
-  // Grab the datetime value from the filter
-  let date = d3.select("#datetime").property("value");
-  let filteredData = tableData;
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
 
-   // Check to see if a date was entered and filter the
-  // data using that date.
-  if (date) {
-    // Apply `filter` to the table data to only keep the
-    // rows where the `datetime` value matches the filter value
-    filteredData = filteredData.filter(row => row.datetime === date);
+// 3. Use this function to update the filters. 
+function updateFilters() {
+
+    // 4a. Save the element that was changed as a variable.
+	let changedfilter = d3.select(this);
+	//David Comment: We are taking the output that will given later in the code
+	
+    // 4b. Save the value that was changed as a variable.
+	let changedvalue = changedfilter.property("value");
+	//David Comment: From the element, we are grabbing the value that will be important in updating the filter	
+	
+    // 4c. Save the id of the filter that was changed as a variable.
+	let filterid = changedfilter.attr("id");
+	//David Comment: Similar to what we did earlier, but now we're taking the id
+  
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+	if (changedvalue) {
+		filters[filterid] = changedvalue;
+	}
+	else {
+		delete filters[filterid]
+	}
+//David Comment: Here if we find a changed value we will update our filters table with a new id and value
+  
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
   }
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
+	let filteredData = tableData;
+  
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    if (filters) {
+		for (var key in filters){
+			filteredData = filteredData.filter(row => row[key] === filters[key]);
+		}
+	}  
+	//David Comment: Now that we have the updated filter variable, we will use it to go through each key in the filters variable, and then filtering the table by making sure the value in the id of each row of the table is equal to the value of the id of the filter variable
 
-   // Rebuild the table using the filtered data
-  // @NOTE: If no date was entered, then filteredData will
-  // just be the original tableData.
-  buildTable(filteredData);
-}
-
-// Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click", handleClick);
-
-// Build the table when the page loads
-buildTable(tableData);
+    // 10. Finally, rebuild the table using the filtered data
+	buildTable(filteredData);
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
